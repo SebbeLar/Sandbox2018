22ce5bc1c9e382cc667f8d307d0d46a42de4dbbe
Sebastian Larsson <sebberlarsson@hotmail.com>
2017-11-21 22:36:31 +0100

    Testing stuff

--- Gatsby/tutorial-part-one/src/pages/index.js
+++ Gatsby/tutorial-part-one/src/pages/index.js
@@ -1,3 +1,9 @@
-import React from "react"
+import React from "react";
+import Link from 'gatsby-link';
 
-export default () => <div>Hello world!</div>
+export default () => 
+<div>
+  <h1>Hello world!</h1>
+  <p style={{ color: `blue` }}>This is a cool Gatsby App</p>
+  <Link to="/page-2/">Page 2</Link>
+ </div>
